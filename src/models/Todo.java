f77202c35dc4d908cbf905e2911f2f2f1c3be26f
KristianMoncada <118566826+KristianMoncada@users.noreply.github.com>
2025-12-09 22:17:00 -0500

    Worked on Create and Read services

--- src/models/Todo.java
+++ src/models/Todo.java
@@ -1,5 +1,39 @@
 package models;
 
 public class Todo {
+    private int id;
+    private boolean isCompleted;
+    private String title;
+
+    public Todo(int id, boolean isCompleted, String title) {
+        this.id = id;
+        this.isCompleted = isCompleted;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean getIsBoolean() {
+        return isCompleted;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     
 }
--- src/services/TodoService.java
+++ src/services/TodoService.java
@@ -1,5 +1,34 @@
 package services;
 
+import java.util.ArrayList;
+import models.Todo;
+
 public class TodoService {
-    
+    ArrayList<Todo> todoList = new ArrayList<>();
+
+    int todoListCounter = 0;
+    public void createTodo(String title) {
+        todoListCounter++;
+        Todo todoTask = new Todo(todoListCounter, false, title);
+        todoList.add(todoTask);
+    }
+
+    public List<Todo> readAllTodo() {
+        if (todoList.isEmpty()) {
+            System.out.println("List is empty there are no tasks");
+            return todoList;
+        } else {
+            return todoList;
+        }
+    }
+
+    public Todo readSpecificTodo(int id) {
+        for (Todo e: todoList) {
+            if (id == e.getId()) {
+                return e;
+            }
+        }
+
+        return null;
+    }
 }
