1afa2a84358bb35f7f76b59b87edb9a4cc7c3dd7
KristianMoncada <118566826+KristianMoncada@users.noreply.github.com>
2025-12-14 21:26:06 -0500

    Finished last changes

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+import ui.TodoAppFx;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(TodoAppFx.class, args);
+    }
+}
